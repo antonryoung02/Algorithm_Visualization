f4c52a9117ce9bd569618791a154069356593c1d
Anton Roman Young <89807629+antonryoung02@users.noreply.github.com>
2024-06-02 18:54:56 -0600

    Minor fixes for Positioners

--- Recursion/PositionStrategies.py
+++ Recursion/PositionStrategies.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 
 class AbstractPositioner(ABC):
     @abstractmethod
-    def get_subproblem_position(self, elements, current_subproblem, spacing_dict) -> None:
+    def get_subproblem_position(self, elements, current_subproblem) -> None:
         pass
 
 class TwoChildrenPositioner(AbstractPositioner):
@@ -10,35 +10,27 @@ class TwoChildrenPositioner(AbstractPositioner):
         self.vertical_spacing = vertical_spacing
         self.horizontal_spacing = horizontal_spacing
 
-
     def get_subproblem_position(self, elements, current_subproblem):
         parent = current_subproblem.parent
         if parent is None:
             return [0, 3, 0]
 
         total_sibling_width = 0.5 * elements[0].get_width() * len(parent.elements) * self.horizontal_spacing
-
-        start_x = parent.get_center()[0] - (total_sibling_width) 
-        x = start_x + (len(parent.children)) * 2 * total_sibling_width
-
+        start_x = parent.get_center()[0] - total_sibling_width
+        x = start_x + len(parent.children) * 2 * total_sibling_width
         y = parent.get_center()[1] - self.vertical_spacing
-
         return [x, y, 0]
     
 class OneChildPositioner(AbstractPositioner):
-    def __init__(self, level_spacing, subproblem_spacing, subproblem_size):
-        self.level_spacing = level_spacing
-        self.subproblem_spacing = subproblem_spacing
-        self.subproblem_size = subproblem_size
+    def __init__(self, vertical_spacing = 1.5):
+        self.vertical_spacing = vertical_spacing
 
-    def get_subproblem_position(self, elements, current_subproblem, level):
+    def get_subproblem_position(self, elements, current_subproblem):
         parent = current_subproblem.parent
-        branching_factor = 2
         if parent is None:
-            return [0, 2, 0]
+            return [0, 3, 0]
 
-        x = parent.get_center()[0] 
-        y = parent.get_center()[1] - 1.5
-
+        x = parent.get_center()[0]
+        y = parent.get_center()[1] - self.vertical_spacing
         return [x, y, 0]
     
